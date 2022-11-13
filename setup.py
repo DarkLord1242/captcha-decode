019d4471b3c35bb85e98d40b895f5d5a6948cd9b
DarkLord1242 <117990790+DarkLord1242@users.noreply.github.com>
2022-11-13 21:48:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+from collections import Counter
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
